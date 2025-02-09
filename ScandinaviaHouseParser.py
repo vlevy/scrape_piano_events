d3812babb419f2439d5892963a18d9e46b661dfd
vlevy <github201@okso.org>
2025-02-09 13:33:49 -0500

    Scandinavia House update

--- ScandinaviaHouseParser.py
+++ ScandinaviaHouseParser.py
@@ -22,9 +22,9 @@ class ScandinaviaHouseParser(EventParser):
         csv_dict["venue_name"] = "Scandinavia House"
 
         # Event name
-        event_name_parts = soup.find_all("h1", attrs={"class": "entry-title"})[
-            0
-        ].contents
+        event_name_parts = soup.find_all(
+            "h1", attrs={"class": "tribe-events-single-event-title"}
+        )[0].contents
 
         event_name_part_1 = str(event_name_parts[0])
         if len(event_name_parts) > 1 and event_name_parts[1].contents:
@@ -38,43 +38,65 @@ class ScandinaviaHouseParser(EventParser):
         event_name = " ".join([word.capitalize() for word in event_name.split()])
         csv_dict["event_name"] = event_name
 
-        # Date: 'Thu—5-2-2019'
-        date_str = str(
-            soup.find_all("div", attrs={"class": "slide-event--large_date"})[
-                0
-            ].contents[0]
-        )
-        date_str = re.search("(?P<date>\d.+$)", date_str).group("date")
-        start_str = f"{date_str} 20:00"
-        try:
-            start_dt = dt.datetime.strptime(start_str, "%m-%d-%Y %H:%M")
-        except Exception as ex:
-            logger.info(
-                f"Warning: Unable to determine starting time from  date: {repr(start_str)}"
+        # Date and start time March 25, 7:30 pm, 2024'
+        date_time_start = (
+            (
+                str(
+                    soup.find_all("span", attrs={"class": "tribe-event-date-start"})[
+                        0
+                    ].contents[0]
+                )
+                .replace("—", "-")
+                .replace("-", ", ")
             )
-            return None
-        else:
-            set_start_end_fields_from_start_dt(csv_dict, start_dt)
+            + ", "
+            + dt.date.today().strftime("%Y")
+        )
+        # String to parse looks like this: March 25, 7:30 pm, 2024
+        start_dt = dt.datetime.strptime(date_time_start, "%B %d, %I:%M %p, %Y")
+        if start_dt < dt.datetime.now():
+            start_dt = start_dt.replace(year=dt.datetime.now().year + 1)
+
+        set_start_end_fields_from_start_dt(csv_dict, start_dt)
 
         # Event description
         paragraphs = [
             str(p)
-            for p in soup.find("div", attrs={"class": ("event-content")})
-            .contents[1]
-            .find_all("p")
+            for p in soup.find(
+                "div", attrs={"class": ("tribe-events-single-event-description")}
+            ).find_all("p")
         ]
         description = "\n".join(paragraphs)
         csv_dict["event_description"] = description
 
         # Price
         logger.info("WARNING: Check listing for correct price.")
-        csv_dict["event_cost"] = "20-25"
+        csv_dict["event_cost"] = "30"
 
         # Tags
         set_tags_from_dict(csv_dict, event_tags)
 
         # Image URL
-        image_url = str(soup.find("meta", attrs={"property": "og:image"})["content"])
+        # Example srcset:
+        # 'https://www.scandinaviahouse.org/wp-content/uploads/2023/08/Photo-Jeffrey-Siegel-at-the-piano-1_NEW-WEB-scaled.jpg 2560w'
+        # 'https://www.scandinaviahouse.org/wp-content/uploads/2023/08/Photo-Jeffrey-Siegel-at-the-piano-1_NEW-WEB-640x262.jpg 640w'
+        # 'https://www.scandinaviahouse.org/wp-content/uploads/2023/08/Photo-Jeffrey-Siegel-at-the-piano-1_NEW-WEB-960x393.jpg 960w'
+        # 'https://www.scandinaviahouse.org/wp-content/uploads/2023/08/Photo-Jeffrey-Siegel-at-the-piano-1_NEW-WEB-768x315.jpg 768w'
+        # 'https://www.scandinaviahouse.org/wp-content/uploads/2023/08/Photo-Jeffrey-Siegel-at-the-piano-1_NEW-WEB-1536x629.jpg 1536w'
+        # 'https://www.scandinaviahouse.org/wp-content/uploads/2023/08/Photo-Jeffrey-Siegel-at-the-piano-1_NEW-WEB-2048x839.jpg 2048w'
+        # 'https://www.scandinaviahouse.org/wp-content/uploads/2023/08/Photo-Jeffrey-Siegel-at-the-piano-1_NEW-WEB-200x82.jpg 200w'
+
+        # Split each image entryu into the URL and the width
+        srcset = soup.find("img", attrs={"class": "wp-post-image"})["srcset"].split(
+            ", "
+        )
+        srcset = [
+            (url, int(width.removesuffix("w")))
+            for url, width in [srcset_entry.split(" ") for srcset_entry in srcset]
+        ]
+
+        # Find the image with the largest width
+        image_url = max(srcset, key=lambda x: x[1])[0]
         csv_dict["external_image_url"] = image_url
 
         return csv_dict
