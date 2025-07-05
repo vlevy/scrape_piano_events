286ba4e5fbc970babc2a9db302b5ee7387df9786
vlevy <github201@okso.org>
2025-07-05 15:59:06 -0400

    Birdland parser reflects updated site

--- BirdlandParser.py
+++ BirdlandParser.py
@@ -11,8 +11,48 @@ from parser_common_code import (
 )
 
 
-class BirdlandParser(EventParser):
+def extract_event_datetimes(soup: bs4.BeautifulSoup) -> list[dt.datetime]:
+    """
+    Extracts event date and time information from a BeautifulSoup object and returns a list of datetime objects.
 
+    Parameters:
+        soup (BeautifulSoup): Parsed BeautifulSoup object containing event information.
+
+    Returns:
+        list[datetime]: A list of datetime objects representing each event's date and time.
+
+    Notes:
+        If an event date is more than 6 months in the past relative to the current date, its year is incremented by one.
+    """
+    event_datetimes = []
+
+    current_date = dt.datetime.now()
+
+    for date_time_div in soup.select("div.tw-date-time"):
+        date_str = date_time_div.select_one("span.tw-event-date").get_text(strip=True)
+        time_str = (
+            date_time_div.select_one("span.tw-event-time")
+            .get_text(strip=True)
+            .replace("Show: ", "")
+        )
+
+        # Combine and parse date and time
+        combined_str = f"{date_str} {time_str}"
+        event_datetime = dt.datetime.strptime(combined_str, "%a, %b %d %I:%M %p")
+
+        # Initially parsed datetime has year 1900, replace with current year
+        event_datetime = event_datetime.replace(year=current_date.year)
+
+        # If the event date is more than 6 months in the past, roll forward by one year
+        if (current_date - event_datetime) > dt.timedelta(days=182):
+            event_datetime = event_datetime.replace(year=event_datetime.year + 1)
+
+        event_datetimes.append(event_datetime)
+
+    return event_datetimes
+
+
+class BirdlandParser(EventParser):
     @staticmethod
     def parse_soup_to_event(url, soup):
         """Parses a soup object into a dictionary whose keys are the CSV rows
@@ -25,47 +65,49 @@ class BirdlandParser(EventParser):
         csv_dict["venue_name"] = venue
 
         # Event name
-        page_event_name = str(soup.find("h1", attrs={"class": "tribe-events-single-event-title"}).contents[0])
+        page_event_name = str(
+            soup.find("div", attrs={"class": "tw-name"}).contents[0]
+        ).strip()
         csv_dict["event_name"] = f"{page_event_name} at {venue}"
 
-        # Description
-        csv_dict["event_description"] = str(
-            soup.find("div", attrs={"class": "tribe-events-content"}).find_all("p")[0].contents[0]
-        )
-        # Tags
-        csv_dict["event_tags"] = "Jazz,Ensemble"
-
         # Image
-        csv_dict["external_image_url"] = str(soup.find("img", attrs={"class": "wp-post-image"})["src"])
+        csv_dict["external_image_url"] = str(
+            soup.find("img", attrs={"class": "event-img"})["src"]
+        ).split("?")[0]
 
         # -----------------------------
         # When
         #
+        event_datetimes = extract_event_datetimes(soup)
+        start_dt = event_datetimes[0]
+        set_start_end_fields_from_start_dt(csv_dict, start_dt)
 
-        # 'startdt=2022-09-18T20:30:00&amp;enddt=2022-09-18T22:00:00'
-        match = re.search(
-            "startdt=(?P<start_year>20\d\d)-(?P<start_month>\d\d)-(?P<start_day>\d\d)T(?P<start_hour>\d\d):(?P<start_minute>\d\d):00&amp;"
-            "enddt=(?P<end_year>20\d\d)-(?P<end_month>\d\d)-(?P<end_day>\d\d)T(?P<end_hour>\d\d):(?P<end_minute>\d\d):00",
-            str(soup),
-        )
-        start_dt = dt.datetime(
-            int(match["start_year"]),
-            int(match["start_month"]),
-            int(match["start_day"]),
-            int(match["start_hour"]),
-            int(match["start_minute"]),
-        )
-        end_dt = dt.datetime(
-            int(match["end_year"]),
-            int(match["end_month"]),
-            int(match["end_day"]),
-            int(match["end_hour"]),
-            int(match["end_minute"]),
-        )
+        # Tags
+        if len(event_datetimes) > 1:
+            csv_dict["event_tags"] = "Jazz,Ensemble,Solo,Recurring"
+        else:
+            csv_dict["event_tags"] = "Jazz,Ensemble,Solo"
 
-        set_start_end_fields_from_start_dt(csv_dict, start_dt, end_dt)
+        # Description
+        description_div = soup.select_one("div.tw-description")
+        if description_div:
+            description_text = "".join(
+                str(child) for child in description_div.contents
+            ).strip()
+        else:
+            description_text = ""
+
+        if len(event_datetimes) > 1:
+            description_text += "\n\nThis is a recurring event. All dates:\n"
+
+            # Saturday, August 16 2025, 10:30 PM
+            description_text += "\n".join(
+                [d.strftime("%A, %B %d %Y, %I:%M %p") for d in event_datetimes]
+            )
+
+        csv_dict["event_description"] = description_text
+
         # -----------------------------
-
-        # Price is not available on the new site
+        # Price is not available
 
         return csv_dict
